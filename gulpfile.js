5d4df684455fb417282bde0693438dcac7512c4f
Tahara Ezell <taharaezell@webheadtech.com>
2018-08-14 00:45:59 -0500

    algaecal 75% done

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+//reference packages
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+//create a task to convert sass into css
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// Move javascript files to src folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+});
+// Watch for scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Start server and launch browser
+gulp.task('default', ['js','serve']);
